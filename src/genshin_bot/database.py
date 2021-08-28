988e87d2b5839f6907f15cc172bf340c2c1442cb
mark <markel20002000@gmail.com>
2021-08-28 21:19:20 +0600

    check_same_thread теперь будет False, только если используется база sqlite

--- src/genshin_bot/database.py
+++ src/genshin_bot/database.py
@@ -5,7 +5,7 @@ from .settings import settings
 
 engine = create_engine(
     settings.database_url,
-    connect_args={'check_same_thread': False},
+    connect_args={'check_same_thread': not ("sqlite" in settings.database_url)},
 )
 
 Session = sessionmaker(
