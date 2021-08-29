0a32cdbd7fafed9da04f914a13c0250281173fe3
mark <markel20002000@gmail.com>
2021-08-29 17:59:22 +0600

    добавил новый сервис для работы с таблицей banners

---
+++ src/genshin_bot/services/banners.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+import sqlalchemy.orm
+
+from .. import tables
+
+
+class BannerService:
+    def __init__(self, session: sqlalchemy.orm.Session):
+        self.session = session
+
+    def get_banner_by_name(self, banner_name: str) -> Optional[tables.Banner]:
+        banner = (
+            self.session.query(tables.Banner)
+                .filter(tables.Banner.name == banner_name)
+                .first()
+        )
+        return banner
+
+    def get_list(self) -> list[tables.Banner]:
+        banners = (
+            self.session.query(tables.Banner)
+                .all()
+        )
+        return banners
