92238bb1b8de304f1f6286e56127e42516981b0a
mark <markel20002000@gmail.com>
2021-08-31 13:38:17 +0600

    теперь для управления БД используется модуль sqlalchemy_utils

--- src/scripts/create_db.py
+++ src/scripts/create_db.py
@@ -1,5 +1,11 @@
+from loguru import logger
+from sqlalchemy_utils.functions import database_exists, create_database
+
 from genshin_bot.database import engine
-from genshin_bot.tables import Base
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        logger.info("База данных создана")
+    else:
+        logger.info("База данных уже существует")
