060649e48235f5c431d547a3b6fafdba2bc32c45
mark <markel20002000@gmail.com>
2021-09-01 23:27:52 +0600

    новые тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,19 @@
 import pytest
 
-from genshin_bot.services.character_roller import DefaultStarRandomizer, DefaultStarRoller
+from genshin_bot.database import Session
+from genshin_bot.services.character_roller import DefaultStarRandomizer, DefaultStarRoller, RedisGuaranteeDropCounter
+from genshin_bot.services.users import BotUsersService
 
 
 @pytest.fixture(scope="session")
-def user():
-    class PseudoUser:
-        def __init__(self):
-            self.id = 1
+def bot_users_service(session):
+    return BotUsersService(session)
 
-    return PseudoUser()
+
+@pytest.fixture(scope="session")
+def bot_user(bot_users_service):
+    _, user = bot_users_service.get_or_create(1, "test_user")
+    return user
 
 
 @pytest.fixture(scope="session")
@@ -18,5 +22,19 @@ def default_star_randomizer():
 
 
 @pytest.fixture(scope="session")
-def default_star_roller(user):
-    return DefaultStarRoller(user)
+def default_star_roller():
+    return DefaultStarRoller()
+
+
+@pytest.fixture(scope="session")
+def session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+@pytest.fixture(scope="session")
+def guarantee_drop_counter(bot_user):
+    return RedisGuaranteeDropCounter(bot_user)
--- tests/test_randomizer.py
+++ tests/test_randomizer.py
@@ -9,16 +9,24 @@ def test_random_distribution(default_star_randomizer):
     assert 0.050 < rolls.count(Rarity.FOUR) / rolls_amount < 0.052, "Шанс выпадения четвертого дропа равен ~5.1%"
 
 
-def test_five_drop_guarantee(default_star_roller):
+def test_five_drop_guarantee(default_star_roller, bot_user, default_star_randomizer, guarantee_drop_counter):
     for _ in range(100):
-        rolls = [default_star_roller.roll_star() for __ in range(90)]
+        rolls = [default_star_roller.roll_star(
+            bot_user,
+            default_star_randomizer,
+            guarantee_drop_counter
+        ) for __ in range(90)]
         assert len(rolls) == 90
         assert Rarity.FIVE in rolls, "За 90 роллов должен гарантированно упасть хотя бы 1 пятый дроп"
 
 
-def test_four_drop_guarantee(default_star_roller):
+def test_four_drop_guarantee(default_star_roller, bot_user, default_star_randomizer, guarantee_drop_counter):
     for _ in range(100):
-        rolls = [default_star_roller.roll_star() for __ in range(10)]
+        rolls = [default_star_roller.roll_star(
+            bot_user,
+            default_star_randomizer,
+            guarantee_drop_counter
+        ) for __ in range(10)]
         assert len(rolls) == 10
         assert Rarity.FOUR in rolls or Rarity.FIVE in rolls, \
             "За 10 роллов должен упасть упасть гарантированно как минимум четвертый дроп"
