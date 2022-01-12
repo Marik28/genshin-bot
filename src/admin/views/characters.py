87fb113e0edaac636a79729885e97f197ed78a65
mark <markel20002000@gmail.com>
2022-01-13 02:54:10 +0600

    improve characters views

--- src/admin/views/characters.py
+++ src/admin/views/characters.py
@@ -1,16 +1,30 @@
+from flask_admin.model.template import macro
+
 from genshin_bot.models import Rarity, Element, Weapon, Sex, Area
-from genshin_bot.tables import CharacterImage
+from genshin_bot.tables import CharacterImage, Banner
 from .base import BaseModelView
 from ..services.choices import make_choices
 
 
 class CharactersView(BaseModelView):
-    inline_models = [CharacterImage]
-    column_details_list = ["name", "rarity", "element", "weapon", "sex", "area", "banners"]
+    inline_models = [CharacterImage, Banner]
+    column_details_list = ["name", "rarity", "element", "weapon", "sex", "area", "banners", "images"]
+    column_searchable_list = ["name"]
+    column_list = ["name", "rarity", "weapon", "element"]
+    column_default_sort = "name"
     form_choices = {
         "rarity": make_choices(Rarity),
         "element": make_choices(Element),
         "weapon": make_choices(Weapon),
         "sex": make_choices(Sex),
         "area": make_choices(Area),
+    }
+    details_template = "characters/details.html"
+    list_template = "characters/list.html"
+    column_formatters_detail = {
+        "images": macro("render_character_image"),
+        "banners": macro("render_character_banner"),
+    }
+    column_formatters = {
+        "name": macro("render_character_name")
     }
