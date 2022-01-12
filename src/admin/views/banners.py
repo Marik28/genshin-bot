ebc432cf07f651b2c13364a79d6f12919f1fbc5b
mark <markel20002000@gmail.com>
2022-01-13 02:54:18 +0600

    improve banners views

--- src/admin/views/banners.py
+++ src/admin/views/banners.py
@@ -1,5 +1,12 @@
+from flask_admin.model.template import macro
+
 from .base import BaseModelView
 
 
 class BannersView(BaseModelView):
     column_details_list = ["name", "characters"]
+    column_formatters = {"name": macro("render_banner_name")}
+    column_formatters_detail = {"characters": macro("render_banner_characters")}
+
+    list_template = "banners/list.html"
+    details_template = "banners/details.html"
