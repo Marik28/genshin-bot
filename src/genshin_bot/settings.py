acd710ec4c094065c96561f53fb4de2cdc9f0110
mark <markel20002000@gmail.com>
2022-01-13 02:38:09 +0600

    add settings

--- src/genshin_bot/settings.py
+++ src/genshin_bot/settings.py
@@ -18,6 +18,10 @@ class Settings(BaseSettings):
     basic_auth_password: str
     admin_url: str = "/genshin/admin/"
 
+    characters_url: str = "https://genshin-impact.fandom.com" \
+                          "/ru/wiki/%D0%9F%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%B6%D0%B8"
+    genshin_wiki_base_url: str = "https://genshin-impact.fandom.com"
+
     base_dir: Path = Path(__file__).resolve().parent.parent.parent
 
     database_url: str = f"sqlite:///{base_dir / 'db.sqlite3'}"
